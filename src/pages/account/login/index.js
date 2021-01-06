0eae327e58deef768027264b963177df5befa946
yangyipeng <578705414@qq.com>
2021-01-06 23:38:10 +0800

    react-native-elements

--- src/pages/account/login/index.js
+++ src/pages/account/login/index.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
 import {Image, StatusBar, Text, View} from 'react-native';
 
-import bg from '../../../res/profileBackground.jpg'
+import bg from '../../../res/profileBackground.jpg';
+import {pxToDp} from '../../../utils/stylesKits';
+import {Input} from 'react-native-elements';
 
 class Login extends Component {
+
+
+  phoneNumberChangeText=()=>{
+
+  }
+
+  renderLogin = () => {
+    // const {phoneNumber, phoneValid} = this.state;
+    return <View>
+      <View><Text style={{fontSize: pxToDp(25), color: '#888', fontWeight: 'bold'}}>手机号登录注册</Text></View>
+      <Input
+        placeholder='请输入手机号码'
+        maxLength={11}
+        keyboardType={'phone-pad'}
+        // value={phoneNumber}
+        inputStyle={{ color: "#333" }}
+        onChangeText={this.phoneNumberChangeText}
+      >
+      </Input>
+    </View>;
+
+  };
+
   render() {
     return (
       <View>
         <StatusBar backgroundColor={'transparent'} translucent={true}/>
-        <Image source={bg}/>
+        <Image style={{width: '100%', height: pxToDp(220)}} source={require('../../../res/profileBackground.jpg')}/>
+        {this.renderLogin()}
       </View>
     );
   }
